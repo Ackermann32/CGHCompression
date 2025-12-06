9b41e01ff01fce07d8d027463a223aa23519fc68
acalifano <a.califano123@gmail.com>
2025-12-06 18:22:39 +0100

    Code Refactor of RPT

--- src/rpt_implementation.py
+++ src/rpt_implementation.py
@@ -11,57 +11,9 @@ import json
 from hologram_visualization.hologram_reconstruction import *
 from hologram_visualization.phase_and_amplitude_reconstruction import *
 import pickle
+from utils.utils import mobius, isPrime, divisors
 
-ORIGINAL_CGH_FILENAME = 'Hol_2D_dice'
-
-def isPrime(n) :
-
-    if (n < 2) :
-        return False
-    for i in range(2, n + 1) :
-        if (i * i <= n and n % i == 0) :
-            return False
-    return True
-
-def mobius(N) :
-    
-    # Base Case
-    if (N == 1) :
-        return 1
-
-    # For a prime factor i 
-    # check if i^2 is also
-    # a factor.
-    p = 0
-    for i in range(1, N + 1) :
-        if (N % i == 0 and 
-                isPrime(i)) :
-
-            # Check if N is
-            # divisible by i^2
-            if (N % (i * i) == 0) :
-                return 0
-            else :
-
-                # i occurs only once, 
-                # increase p
-                p = p + 1
-
-    # All prime factors are
-    # contained only once
-    # Return 1 if p is even
-    # else -1
-    if(p % 2 != 0) :
-        return -1
-    else :
-        return 1
-
-def divisors(N):
-    divs = []
-    for d in range(1, N + 1):
-        if N % d == 0:
-            divs.append(d)
-    return divs
+ORIGINAL_CGH_FILENAME = 'Hol_3D_venus'
 
 def euler_phi(q): #Restituisce quanti numeri tra 1 e q sono coprimi con q
     count = 0
@@ -71,15 +23,16 @@ def euler_phi(q): #Restituisce quanti numeri tra 1 e q sono coprimi con q
     return count
 
 
-def calculate_ramanujan_sums(lenght):
+def calculate_ramanujan_sums(lenght, typeRep):
 
     div = divisors(lenght)
+
     
-    P_N = np.zeros((lenght, lenght))
+    P_N = np.zeros((lenght, lenght), dtype=typeRep)
     
     j=0
     for q in div:
-        c_q = np.zeros(q)
+        c_q = np.zeros(q, dtype=np.float64)
 
         #passo 1
         for n in range(q):
@@ -88,7 +41,7 @@ def calculate_ramanujan_sums(lenght):
             for d in range(1, g+1):
                 if (g % d == 0):
                     sum += d*mobius(q//d) #Da ricorda che forse si mette // per la divisione senza virgola, esempio 15:2 mi da 7
-            c_q[n] = sum
+            c_q[n] =  np.float64(sum) if typeRep == np.float64 else np.float32(sum)
         
         #passo 2
         coprims = euler_phi(q)
@@ -99,7 +52,7 @@ def calculate_ramanujan_sums(lenght):
 
         for l in range(coprims):
             c_q_shift = np.roll(c_q, -l)
-            col = np.empty(lenght)
+            col = np.empty(lenght, dtype=typeRep)
             for n in range(lenght):
              #   print(c_q_shift)
                 col[n] = c_q_shift[n % q]
@@ -113,20 +66,25 @@ def calculate_ramanujan_sums(lenght):
             
 def calculate_Y(X):
 
-    P_N = calculate_ramanujan_sums(1080)
-    P_M = calculate_ramanujan_sums(1920)
+    typeRep = np.float64 if X.dtype == 'complex128' else np.float32
+
+    P_N = calculate_ramanujan_sums(X.shape[0], typeRep)
+    P_M = calculate_ramanujan_sums(X.shape[1], typeRep)
 
 
-    F_N_inv = np.linalg.inv(P_N)
-    F_M_inv = np.linalg.inv(P_M)
+    F_N_inv = np.linalg.inv(P_N).astype(typeRep)
+    F_M_inv = np.linalg.inv(P_M).astype(typeRep)
 
     Y = F_N_inv @ X @ F_M_inv.T 
 
     return Y   
 
 def calculate_X(Y):
-    P_N = calculate_ramanujan_sums(1080)
-    P_M = calculate_ramanujan_sums(1920)
+
+    typeRep = np.float64 if Y.dtype == 'complex128' else np.float32
+
+    P_N = calculate_ramanujan_sums(Y.shape[0], typeRep)
+    P_M = calculate_ramanujan_sums(Y.shape[1], typeRep)
 
     X = P_N @ Y @ P_M.T
     return X        
@@ -142,6 +100,8 @@ def compress_with_fpzip(hologram:Hologram,output_file,split=True):
     header_bytes = json.dumps(metadata).encode("utf-8")
     header_len = np.int64(len(header_bytes)).tobytes()
 
+    typeRep = np.float64 if hologram.hol.dtype == 'complex128' else np.float32
+
     with open(output_file , 'wb') as f:
 
         f.write(header_len)
@@ -150,11 +110,11 @@ def compress_with_fpzip(hologram:Hologram,output_file,split=True):
         matrix = hologram.hol
 
         if split and np.iscomplexobj(matrix):
-            real_data = np.ascontiguousarray(np.real(matrix), dtype=np.float64)
-            imag_data = np.ascontiguousarray(np.imag(matrix), dtype=np.float64)
+            real_data = np.ascontiguousarray(np.real(matrix), dtype=typeRep)
+            imag_data = np.ascontiguousarray(np.imag(matrix), dtype=typeRep) 
             
-            compressed_real = fpzip.compress(real_data)
-            compressed_imag = fpzip.compress(imag_data)
+            compressed_real = fpzip.compress(real_data, precision=0, order='C')
+            compressed_imag = fpzip.compress(imag_data, precision=0, order='C')
             #Salvo la lunghezza 
             f.write(np.int64(len(compressed_real)).tobytes())
             f.write(np.int64(len(compressed_imag)).tobytes())
@@ -163,7 +123,7 @@ def compress_with_fpzip(hologram:Hologram,output_file,split=True):
         
         else:
             #Reinterpreto la matrice complessa come una matrice di float64, senza perdere informazione
-            float_view = matrix.view(np.float64)
+            float_view = matrix.view(typeRep)
             float_view = np.ascontiguousarray(float_view)
 
             compressed = fpzip.compress(float_view)
@@ -171,7 +131,6 @@ def compress_with_fpzip(hologram:Hologram,output_file,split=True):
 
 def decompress_with_fpzip(output_file):
     with open(output_file, 'rb') as f:
-
         header_len = np.frombuffer(f.read(8), dtype=np.int64)[0]
         header_bytes = f.read(header_len)
         metadata = json.loads(header_bytes.decode("utf-8"))
@@ -184,16 +143,20 @@ def decompress_with_fpzip(output_file):
             compressed_real = f.read(len_real)
             compressed_imaginary = f.read(len_imag)
 
-            uncompressed_real = fpzip.decompress(compressed_real).squeeze()
-            uncompressed_imaginary = fpzip.decompress(compressed_imaginary).squeeze()
+            uncompressed_real = fpzip.decompress(compressed_real, order='C').squeeze()
+            uncompressed_imaginary = fpzip.decompress(compressed_imaginary, order='C').squeeze()
+
+
 
             #ricostruisco la matrice complessa
             Y = uncompressed_real + 1j * uncompressed_imaginary
+
+
             return Hologram(Y, metadata["pp"], metadata["zobj"], metadata["wlen"])
         else:
             data = f.read()
             float_array = fpzip.decompress(data)
-            float_array = float_array.reshape(1080, 1920, 2)
+            float_array = float_array.reshape(Y.shape[0], Y.shape[1], 2)
 
             # ricostruzione dei complessi
             complex_matrix = float_array[...,0] + 1j * float_array[...,1]
@@ -209,14 +172,20 @@ def decompress_with_fpzip(output_file):
 
 
 def main():
+
+    #Recupero dell'ologramma
     filepath_mat = os.path.join(os.path.dirname(__file__),'..', 'dataset', f'{ORIGINAL_CGH_FILENAME}.mat')
 
     hologram_data = Hologram.open_hologram_file(filepath_mat)
 
-    Y = calculate_Y(hologram_data.hol)
 
+    #Calcolo della trasformata dell'ologramma
+    Y = calculate_Y(hologram_data.hol)
+    
     X = hologram_data.hol
 
+
+    #Si specifica che la compressione deve essere splittata, ovvero comprimere parte reale ed immaginaria in modo separato
     split = True
     output_file = os.path.join(
     os.path.dirname(__file__),
@@ -225,24 +194,35 @@ def main():
     f"{ORIGINAL_CGH_FILENAME}_compressed_rpt{'_unsplitted' if not split else ''}.fpzip"
 )
 
-
+    #Compressione utilizzando fpzip
     compress_with_fpzip(Hologram(Y,hologram_data.pp, hologram_data.zobj, hologram_data.wlen), output_file, split)
 
+    #Decompressione utilizzando fpzip
     decompressed_hologram_data = decompress_with_fpzip(output_file)
-    decompressed_hologram_data.hol = calculate_X(decompressed_hologram_data.hol)
-    decompressed_X = decompressed_hologram_data.hol
 
+    #Ricostruzione dell'ologramma decompresso
+    decompressed_X = calculate_X(decompressed_hologram_data.hol)
+
+    #Calcolo similarità
     similarity_manager = paper_similarity.Similarity(paper_similarity.GammaM.bump, paper_similarity.GammaR.cos,
                                              paper_similarity.GammaA.unique)
 
     similarity = similarity_manager.calc_similarity(X,decompressed_X)
+
+
+
+
     print('Similarity = ',similarity)
+    print("Equality between original and decompressed hologram =", np.array_equal(X, decompressed_X))
+    print("Difference between original and decompressed hologram =", np.max(np.abs(X - decompressed_X)))
+
 
     #Salvo ologramma in formato .raw per calcolare il tasso di compressione
     path_raw = os.path.join(os.path.dirname(__file__),'hologram.raw')
     with open(path_raw, "wb") as fp:
         pickle.dump(hologram_data, fp)
-    print("compression rate =", utils.calculate_compression_rate(output_file, path_raw))
+    print("compression rate =",(1 - utils.calculate_compression_rate(output_file, path_raw))*100)
+    
     os.remove(path_raw)
 
     show_hologram_reconstruction(hologram_data)
